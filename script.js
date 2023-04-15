09bb1a62ad5d71e7ac2e928d47cccf6b986723fe
Ranjeet Singh Rawat <ranjeet843507@gmail.com>
2023-04-16 02:45:39 +0530

    most of the work done

---
+++ script.js
@@ -0,0 +1,35 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "f37b1fd78bmsh37c01d62694ac7dp1099cdjsn30b3b5fb1a54",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+const getWeather = (city) => {
+  cityName.innerHTML = city;
+  fetch(
+    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      temp.innerHTML = response.temp;
+      temp2.innerHTML = response.temp;
+      feels_like.innerHTML = response.feels_like;
+      humidity.innerHTML = response.humidity;
+      min_temp.innerHTML = response.min_temp;
+      max_temp.innerHTML = response.max_temp;
+      wind_speed.innerHTML = response.wind_speed;
+      wind_degrees.innerHTML = response.wind_degrees;
+
+    })
+    .catch((err) => console.error(err));
+};
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  getWeather(city.value);
+});
+
+getWeather("Gwalior");
